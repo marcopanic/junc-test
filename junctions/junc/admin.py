c14cb9184fc28334facb54ef7bd3d956e7a37792
macropanic <macropanic@gmail.com>
2022-06-05 14:13:35 +0200

    junction class

--- junctions/junc/admin.py
+++ junctions/junc/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Junction
 
 # Register your models here.
+
+admin.site.register(Junction)
--- junctions/junc/models.py
+++ junctions/junc/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Junction(models.Model):
+    corr = models.CharField(max_length=2)
+    mark = models.CharField(max_length=4)
+    status = models.CharField(max_length=20)
+    needs = models.CharField(max_length=20)
+    civil_works_need = models.BooleanField(default=False)
+    add_info = models.TextField(max_length=110)
+
+    def __str__(self):
+        return '{} - {}'.format(self.corr, self.mark)
+    
